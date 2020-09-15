32bb2a1c709592b7fa6e8f78497af118aac7c8d2
matthew balagtas <matthew.balagtas007@gmail.com>
2020-09-15 12:08:53 +0800

    added roll dice and players

--- app.js
+++ app.js
@@ -8,3 +8,42 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores       = [0,0];
+let roundScore   = 0;
+let activePlayer = 0; 
+document.getElementById('score-0').textContent ='0';
+document.getElementById('score-1').textContent ='0';
+document.getElementById('current-0').textContent ='0';
+document.getElementById('current-1').textContent ='0';
+document.querySelector('.dice').style.display = 'none';
+const roller = document.querySelector('.btn-roll');
+
+
+ roller.addEventListener('click', function() {
+    var dice = Math.floor(Math.random()*6) + 1;
+ 	var diceDom  = document.querySelector('.dice');
+
+ 	diceDom.style.display = 'block';
+ 	diceDom.src = 'dice-' + dice + '.png';
+
+ 	if(dice !== 1){
+ 		//add score
+ 		roundScore += dice;
+ 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+ 	}else{ 
+ 		//nextplayer
+ 		// activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		// roundScore = 0
+			 if (activePlayer === 0) {
+				 activePlayer = 1;
+			 }else{
+				 activePlayer = 0;
+			 }
+ 		document.getElementById('current-0').textContent = '0'; 
+ 		document.getElementById('current-1').textContent = '0'
+ 		document.querySelector('.player-0-panel').classList.toggle('active');
+ 		document.querySelector('.player-1-panel').classList.toggle('active');
+ 		// document.querySelector('.dice').style.display = 'none'
+ 		}
+ });
